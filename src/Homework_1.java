a43a3849e177733658e7688237b7cafffc08038f
MaksimMonro-create <maksim.monro@yandex.ru>
2020-10-17 16:29:45 +0300

    Initial commit

---
+++ src/Homework_1.java
@@ -0,0 +1,70 @@
+public class Homework_1 {
+
+    public class Lesson1_1 {
+        public static void main (String args[]) {
+            System.out.println ("Домашнее задание № 1");
+        }
+    }
+
+    public class Lesson1_2 {
+        public static void main (String args[]) {
+            int a = 5; // От -2147483648 до 2147483647, объявление переменной с инициализацией
+            short b = 20; // От -32768 до 32767, объявление переменной с инициализацией
+            byte c = 0; // От -128 до 127, объявление переменной с инициализацией
+            long d = 678L; // От -9223372036854775807 до 9223372036854775807, объявление переменной с инициализацией
+            float e = 12.45f; // Объявление переменной с инициализацией
+            double f = -123.345; // Объявление переменной с инициализацией
+            char g = '*'; // Объявление переменной с инициализацией
+            boolean val = true; // Объявление переменной с инициализацией
+        }
+
+        public class Lesson1_3 {
+            public static int do_something (int a, int b, int c, int d) {
+                return a * (b + (c / d))
+            }
+        }
+
+        public class Lesson1_4 {
+            public static boolean main (String[] args) {
+                int a;
+                int b;
+                int c = a + b;
+                if (20 >= c > 10) {
+                    return true; {
+                    } else {
+                        return false;
+                    }
+
+                }
+
+            }
+        }
+
+        public class Lesson1_5 {
+            public static void main (String args[]) {
+                int a;
+                if (a >= 0) {
+                    System.out.println("Введенное число положительное");
+                }
+                if (a < 0) {
+                    System.out.println("Введенное число отрицательное");
+                }
+            }
+        }
+
+        public class Lesson1_6 {
+            public static int a {
+                if (a < 0) {
+                    return true;
+                }
+            }
+        }
+
+        public class Lesson1_7 {
+            public static void main (String args []) {
+                string a = "Привет, указанное имя";
+                System.out.println("a");
+            }
+        }
+        
+}
